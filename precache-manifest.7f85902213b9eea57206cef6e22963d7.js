6b234f26289242303a53f96ffcb77132f3e152fa
arun4other <arun4other@gmail.com>
2020-11-08 22:33:56 +0530

    Updates

---
+++ precache-manifest.7f85902213b9eea57206cef6e22963d7.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bacdaca67abe20a0c2f5bf3ffd01abb0",
+    "url": "/daatkhor/index.html"
+  },
+  {
+    "revision": "2741a3ba94756f8b5cac",
+    "url": "/daatkhor/static/css/main.ca7426a0.chunk.css"
+  },
+  {
+    "revision": "9dadd1a5607abc36395e",
+    "url": "/daatkhor/static/js/2.db3844ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/daatkhor/static/js/2.db3844ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2741a3ba94756f8b5cac",
+    "url": "/daatkhor/static/js/main.690d1804.chunk.js"
+  },
+  {
+    "revision": "ce7327c7d170e5a67388",
+    "url": "/daatkhor/static/js/runtime-main.eaa60968.js"
+  },
+  {
+    "revision": "24721b39bb80e1867acf896628286223",
+    "url": "/daatkhor/static/media/Beautiful.24721b39.jpg"
+  },
+  {
+    "revision": "dbd71e7141d3b1cccc350ce9c89cdeca",
+    "url": "/daatkhor/static/media/Daatkhor_bhukkhad.dbd71e71.jpg"
+  },
+  {
+    "revision": "30da7705c79b0843cc19b35df81a4618",
+    "url": "/daatkhor/static/media/Darawani_Aurat.30da7705.jpg"
+  },
+  {
+    "revision": "62d1f94147b0627a7c9ec69725d09f91",
+    "url": "/daatkhor/static/media/Dhuski.62d1f941.jpg"
+  },
+  {
+    "revision": "e5a2d676be75ab0123ac2e2aeb27bba7",
+    "url": "/daatkhor/static/media/Maasum_bacchi.e5a2d676.jpg"
+  },
+  {
+    "revision": "6fb01c4ea71885ff7de92974dfd9deb1",
+    "url": "/daatkhor/static/media/Stylish_Aurat.6fb01c4e.jpg"
+  },
+  {
+    "revision": "c5a7d52ebece154347777874a41edf45",
+    "url": "/daatkhor/static/media/Vampire.c5a7d52e.jpg"
+  },
+  {
+    "revision": "87c43c011cc8100403ddf8d406af72c8",
+    "url": "/daatkhor/static/media/dp.87c43c01.jpg"
+  },
+  {
+    "revision": "d1d06f0ee6adfd7a179f9429c9d10912",
+    "url": "/daatkhor/static/media/fav.d1d06f0e.jpg"
+  },
+  {
+    "revision": "56c5718c37195c8bf1478de28494df86",
+    "url": "/daatkhor/static/media/haaye.56c5718c.jpg"
+  },
+  {
+    "revision": "878349f90a54413f67fcb337b82f365f",
+    "url": "/daatkhor/static/media/kya_kehna.878349f9.jpg"
+  },
+  {
+    "revision": "d2811340a46b85d46aa9c325217c271d",
+    "url": "/daatkhor/static/media/naakti.d2811340.jpg"
+  },
+  {
+    "revision": "c79461689f992d7513075d0bbd56808d",
+    "url": "/daatkhor/static/media/pout.c7946168.jpg"
+  },
+  {
+    "revision": "75893dee093689cb2d6270d43eedd2fe",
+    "url": "/daatkhor/static/media/saree.75893dee.jpg"
+  },
+  {
+    "revision": "9f452c7b0d99194e8f3472b778dc709d",
+    "url": "/daatkhor/static/media/saree_me_nakhra.9f452c7b.jpg"
+  }
+]);
