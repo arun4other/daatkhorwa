5f55c0ec9f7bcad7f367dabaa10e18d4c32e0ae4
arun4other <arun4other@gmail.com>
2020-11-10 21:19:24 +0530

    added new images

--- src/App.js
+++ src/App.js
@@ -11,244 +11,172 @@ class App extends Component{
     this.state = {
       monsters:[
         {
+          "id": 21,
+          "name": "Meri Maa (love)",
+          "username": "mom",
+          "imgUrl": "../../Assets/images/Vampire.jpg"
+        },
+        {
+          "id": 22,
+          "name": "Mere papa (second love)",
+          "username": "papa",
+          "imgUrl": "../../Assets/images/Vampire.jpg"
+        },
+        {
           "id": 1,
           "name": "Beautiful",
           "imgUrl": '../../Assets/images/Beautiful.jpg',
           "username": "Beautiful",
-          "email": "hasnewali@april.biz",
-          "address": {
-            "street": "Kulas Light",
-            "suite": "Apt. 556",
-            "city": "Gwenborough",
-            "zipcode": "92998-3874",
-            "geo": {
-              "lat": "-37.3159",
-              "lng": "81.1496"
-            }
-          },
-          "phone": "1-770-736-8031 x56442",
-          "website": "hildegard.org",
-          "company": {
-            "name": "Romaguera-Crona",
-            "catchPhrase": "Multi-layered client-server neural-net",
-            "bs": "harness real-time e-markets"
-          }
+          "email": "hasnewali@april.biz"
         },
         {
           "id": 2,
           "name": "pad rokne ki ninja technique",
           "username": "Dhuski",
           "imgUrl": "../../Assets/images/Dhuski.jpg",
-          "email": "Shanna@melissa.tv",
-          "address": {
-            "street": "Victor Plains",
-            "suite": "Suite 879",
-            "city": "Wisokyburgh",
-            "zipcode": "90566-7771",
-            "geo": {
-              "lat": "-43.9509",
-              "lng": "-34.4618"
-            }
-          },
-          "phone": "010-692-6593 x09125",
-          "website": "anastasia.net",
-          "company": {
-            "name": "Deckow-Crist",
-            "catchPhrase": "Proactive didactic contingency",
-            "bs": "synergize scalable supply-chains"
-          }
+          "email": "Shanna@melissa.tv"
         },
         {
           "id": 3,
           "name": "Daatkhor Bhukkad",
           "username": "Daatkhor_bhukkhad",
           "email": "Nathan@yesenia.net",
-          "imgUrl": "../../Assets/images/Daatkhor_bhukkhad.jpg",
-          "address": {
-            "street": "Douglas Extension",
-            "suite": "Suite 847",
-            "city": "McKenziehaven",
-            "zipcode": "59590-4157",
-            "geo": {
-              "lat": "-68.6102",
-              "lng": "-47.0653"
-            }
-          },
-          "phone": "1-463-123-4447",
-          "website": "ramiro.info",
-          "company": {
-            "name": "Romaguera-Jacobson",
-            "catchPhrase": "Face to face bifurcated interface",
-            "bs": "e-enable strategic applications"
-          }
+          "imgUrl": "../../Assets/images/Daatkhor_bhukkhad.jpg"
         },
         {
           "id": 4,
           "name": "Darawani Aurat",
           "username": "Darawani_Aurat",
-          "imgUrl": "../../Assets/images/Darawani_Aurat.jpg",
-          "email": "Julianne.OConner@kory.org",
-          "address": {
-            "street": "Hoeger Mall",
-            "suite": "Apt. 692",
-            "city": "South Elvis",
-            "zipcode": "53919-4257",
-            "geo": {
-              "lat": "29.4572",
-              "lng": "-164.2990"
-            }
-          },
-          "phone": "493-170-9623 x156",
-          "website": "kale.biz",
-          "company": {
-            "name": "Robel-Corkery",
-            "catchPhrase": "Multi-tiered zero tolerance productivity",
-            "bs": "transition cutting-edge web services"
-          }
-        },
-        {
-          "id": 5,
-          "name": "Haaye ..;)",
-          "username": "haaye",
-          "email": "Lucio_Hettinger@annie.ca",
-          "imgUrl": "../../Assets/images/haaye.jpg",
-          "address": {
-            "street": "Skiles Walks",
-            "suite": "Suite 351",
-            "city": "Roscoeview",
-            "zipcode": "33263",
-            "geo": {
-              "lat": "-31.8129",
-              "lng": "62.5342"
-            }
-          },
-          "phone": "(254)954-1289",
-          "website": "demarco.info",
-          "company": {
-            "name": "Keebler LLC",
-            "catchPhrase": "User-centric fault-tolerant solution",
-            "bs": "revolutionize end-to-end systems"
-          }
+          "imgUrl": "../../Assets/images/Darawani_Aurat.jpg"
         },
         {
           "id": 6,
           "name": "Kya Kehna ",
           "username": "kya_kehna",
           "email": "Karley_Dach@jasper.info",
-          "imgUrl": "../../Assets/images/kya_kehna.jpg",
-          "address": {
-            "street": "Norberto Crossing",
-            "suite": "Apt. 950",
-            "city": "South Christy",
-            "zipcode": "23505-1337",
-            "geo": {
-              "lat": "-71.4197",
-              "lng": "71.7478"
-            }
-          },
-          "phone": "1-477-935-8478 x6430",
-          "website": "ola.org",
-          "company": {
-            "name": "Considine-Lockman",
-            "catchPhrase": "Synchronised bottom-line interface",
-            "bs": "e-enable innovative applications"
-          }
+          "imgUrl": "../../Assets/images/kya_kehna.jpg"
         },
         {
           "id": 7,
           "name": "Maasum Bacchi",
           "username": "Maasum_bacchi",
-          "imgUrl": "../../Assets/images/Maasum_bacchi.jpg",
-          "email": "Telly.Hoeger@billy.biz",
-          "address": {
-            "street": "Rex Trail",
-            "suite": "Suite 280",
-            "city": "Howemouth",
-            "zipcode": "58804-1099",
-            "geo": {
-              "lat": "24.8918",
-              "lng": "21.8984"
-            }
-          },
-          "phone": "210.067.6132",
-          "website": "elvis.io",
-          "company": {
-            "name": "Johns Group",
-            "catchPhrase": "Configurable multimedia task-force",
-            "bs": "generate enterprise e-tailers"
-          }
+          "imgUrl": "../../Assets/images/Maasum_bacchi.jpg"
         },
         {
           "id": 8,
           "name": "Naakti Dikhane ka style",
           "username": "naakti",
-          "imgUrl": "../../Assets/images/naakti.jpg",
-          "email": "Sherwood@rosamond.me",
-          "address": {
-            "street": "Ellsworth Summit",
-            "suite": "Suite 729",
-            "city": "Aliyaview",
-            "zipcode": "45169",
-            "geo": {
-              "lat": "-14.3990",
-              "lng": "-120.7677"
-            }
-          },
-          "phone": "586.493.6943 x140",
-          "website": "jacynthe.com",
-          "company": {
-            "name": "Abernathy Group",
-            "catchPhrase": "Implemented secondary concept",
-            "bs": "e-enable extensible e-tailers"
-          }
+          "imgUrl": "../../Assets/images/naakti.jpg"
         },
         {
           "id": 9,
           "name": "Stylish Aurat",
           "username": "Stylish_Aurat",
           "email": "Chaim_McDermott@dana.io",
-          "imgUrl": "../../Assets/images/Stylish_Aurat.jpg",
-          "address": {
-            "street": "Dayna Park",
-            "suite": "Suite 449",
-            "city": "Bartholomebury",
-            "zipcode": "76495-3109",
-            "geo": {
-              "lat": "24.6463",
-              "lng": "-168.8889"
-            }
-          },
-          "phone": "(775)976-6794 x41206",
-          "website": "conrad.com",
-          "company": {
-            "name": "Yost and Sons",
-            "catchPhrase": "Switchable contextually-based project",
-            "bs": "aggregate real-time technologies"
-          }
+          "imgUrl": "../../Assets/images/Stylish_Aurat.jpg"
         },
         {
           "id": 10,
           "name": "Vampire",
           "username": "Vampire",
-          "imgUrl": "../../Assets/images/Vampire.jpg",
-          "email": "Rey.Padberg@karina.biz",
-          "address": {
-            "street": "Kattie Turnpike",
-            "suite": "Suite 198",
-            "city": "Lebsackbury",
-            "zipcode": "31428-2261",
-            "geo": {
-              "lat": "-38.2386",
-              "lng": "57.2232"
-            }
-          },
-          "phone": "024-648-3804",
-          "website": "ambrose.net",
-          "company": {
-            "name": "Hoeger LLC",
-            "catchPhrase": "Centralized empowering task-force",
-            "bs": "target end-to-end models"
-          }
+          "imgUrl": "../../Assets/images/Vampire.jpg"
+        },
+        {
+          "id": 11,
+          "name": "Aunties",
+          "username": "aunties",
+          "imgUrl": "../../Assets/images/Vampire.jpg"
+        },
+        {
+          "id": 12,
+          "name": "Bade Papa",
+          "username": "bade_papa",
+          "imgUrl": "../../Assets/images/Vampire.jpg"
+        },
+        {
+          "id": 13,
+          "name": "Bhabhi",
+          "username": "bhabhi",
+          "imgUrl": "../../Assets/images/Vampire.jpg"
+        },
+        {
+          "id": 14,
+          "name": "Bhayander Beach",
+          "username": "bhayander",
+          "imgUrl": "../../Assets/images/Vampire.jpg"
+        },
+        {
+          "id": 15,
+          "name": "Guddu Chacha",
+          "username": "chacha",
+          "imgUrl": "../../Assets/images/Vampire.jpg"
+        },
+        {
+          "id": 16,
+          "name": "Ranjana Chachi",
+          "username": "chachi",
+          "imgUrl": "../../Assets/images/Vampire.jpg"
+        },
+        {
+          "id": 17,
+          "name": "Family",
+          "username": "faamily",
+          "imgUrl": "../../Assets/images/Vampire.jpg"
+        },
+        {
+          "id": 18,
+          "name": "Family",
+          "username": "famili",
+          "imgUrl": "../../Assets/images/Vampire.jpg"
+        },
+        {
+          "id": 19,
+          "name": "Family",
+          "username": "family",
+          "imgUrl": "../../Assets/images/Vampire.jpg"
+        },
+        {
+          "id": 20,
+          "name": "Holi",
+          "username": "holi",
+          "imgUrl": "../../Assets/images/Vampire.jpg"
+        },
+        {
+          "id": 23,
+          "name": "Pinku Bhaiya",
+          "username": "pinku_bhaiya",
+          "imgUrl": "../../Assets/images/Vampire.jpg"
+        },
+        {
+          "id": 24,
+          "name": "Pooja Behna (Chudail)",
+          "username": "pooja",
+          "imgUrl": "../../Assets/images/Vampire.jpg"
+        },
+        {
+          "id": 25,
+          "name": "Shivaansh",
+          "username": "shivansh",
+          "imgUrl": "../../Assets/images/Vampire.jpg"
+        },
+        {
+          "id": 26,
+          "name": "Sonia",
+          "username": "sonia",
+          "imgUrl": "../../Assets/images/Vampire.jpg"
+        },
+        {
+          "id": 27,
+          "name": "Theatre (WAR)",
+          "username": "theatre",
+          "imgUrl": "../../Assets/images/Vampire.jpg"
+        },
+        {
+          "id": 5,
+          "name": "Haaye ..;)",
+          "username": "haaye",
+          "email": "Lucio_Hettinger@annie.ca",
+          "imgUrl": "../../Assets/images/haaye.jpg"
         }
       ],
       searchField: ""
@@ -286,6 +214,7 @@ class App extends Component{
     return(
       <div className = "App">
         <h3>Happy Birthday Diksha (Monster)</h3>
+        <img src={require("./Assets/images/dp.jpg")} className="desktopPicture"/>
         <SearchBox placeHolder="search monster"
           handleChange = {e => {this.setState({searchField:e.target.value})}}
         />
